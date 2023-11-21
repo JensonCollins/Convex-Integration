99c1b38d666a7d1e375090a8fbc23c67343fa684
JensonCollins <jenson.collins@hotmail.com>
2023-11-21 07:57:44 -0500

    add: more test cases

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,9 @@
 import { ethers, network } from "hardhat";
 import { Signer } from "ethers";
 import { Contract } from "ethers";
+import { expect } from "chai";
+import { formatEther, parseEther } from "ethers/lib/utils";
+import { mine, time } from "@nomicfoundation/hardhat-network-helpers";
 
 describe("ConvexVault Test", function () {
   let convexVault: Contract;
@@ -47,5 +50,50 @@ describe("ConvexVault Test", function () {
 
     Booster = await ethers.getContractAt("IBooster", "0xF403C135812408BFbE8713b5A23a04b3D48AAE31");
     await Booster.connect(signer1).earmarkRewards(convexPid);
+  });
+
+  it('Should calculate rewards correctly without vault claim', async () => {
+    const pid = 0;
+    const amount = parseEther("10");
+    const signerAddress = await signer0.getAddress();
+
+    // Approve tokens
+    await lpToken.connect(signer0).approve(convexVault.address, amount);
+    console.log("Approved LP tokens for staking");
+    // Deposit tokens && Stake
+    await convexVault.connect(signer0).deposit(pid, amount);
+    console.log("Deposited into convexVault and stake in the BaseRewardPool: ", formatEther(amount));
+
+    // Get calculated rewards
+    const rewards = await convexVault.calculateRewardsEarned(signerAddress, pid);
+
+    await time.increase(18000);
+    await mine();
+    // Get updated rewards
+    const updatedRewards = await convexVault.calculateRewardsEarned(signerAddress, pid);
+    expect(updatedRewards[0]).to.be.not.equal(rewards[0]);
+    expect(updatedRewards[1]).to.be.not.equal(rewards[1]);
+    console.log("CRV Token Reward: ", formatEther(updatedRewards[0]));
+    console.log("CVX Token Reward: ", formatEther(updatedRewards[1]));
+    console.log("Confirmed rewards were genereated without getVaultRewards");
+  }).timeout(300000);
+
+  it('Should add a new pool', async function () {
+    const allocPoint = 100;
+    const lpToken = "0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2";
+    await convexVault.addPool(allocPoint, lpToken, 0);
+
+    const poolLength = await convexVault.poolLength();
+    expect(poolLength).to.equal(2);
+
+    const poolInfo = await convexVault.poolInfo(1);
+    expect(poolInfo.lpToken).to.equal(lpToken);
+    expect(poolInfo.allocPoint).to.equal(allocPoint);
+  });
+
+  it('Should revert on wrong LP token address', async function () {
+    const allocPoint = 100;
+    const lpToken = "0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2";
+    await expect(convexVault.addPool(allocPoint, lpToken, 1)).to.be.revertedWith("Wrong Pid for Convex");
   });
 });
